9c6ff665fff296157808e191d0d7451d75b0f77c
Madhiyan B <madhiyanb@gmail.com>
2018-03-13 23:40:01 +0530

    Changes to be committed: 	modified:   Mylearning/Comm protocol/J1939.odt 	modified:   Mylearning/Comm protocol/can.odt 	new file:   practice/src/pointers.c

---
+++ practice/src/pointers.c
@@ -0,0 +1,5 @@
+http://www.firmcodes.com/what-are-practical-uses-of-function-pointers-in-c/
+https://stackoverflow.com/questions/8975208/whats-the-use-of-function-pointers
+https://stackoverflow.com/questions/6807376/call-back-routine
+
+https://barrgroup.com/search/all/function%20pointer
